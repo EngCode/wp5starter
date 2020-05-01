77e36a006c123d3eae08bb2e2ed3b6a8d6da340b
engcode <elampratorabdallah14@gmail.com>
2020-05-01 18:44:23 +0200

    Initial commit

--- wp-content/themes/tornado/_form_example.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //======= Get Form Data ======//
-    $pageTit = get_the_title();
-    if ( isset($_GET['utm_source']) ) { $utm_source = $_GET['utm_source']; } else { $utm_source = 'Not Available'; }
-    if ( isset($_GET['utm_campaign']) ) { $utm_campaign = $_GET['utm_campaign'];} else { $utm_campaign = 'Not a Campaign'; }
-    //====== Get the Form ======//
-    echo do_shortcode('[contact-form-7 id="1075" page-title="'.$pageTit.' || المصدر : '.$utm_source.' || الحملة : '.$utm_campaign.'"]');
-?>
